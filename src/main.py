92f19f50d04502cdd4f517a8f03e8a4fd0487173
Florin Bicher <f.bicher@gmail.com>
2025-10-19 17:12:03 +0200

    Enhance email sending functionality in main.py by adding error handling for email failures using RequestException.

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 from dotenv import load_dotenv
+from requests import RequestException
 
 from notifier import send_email
 from scraper import check_player_has_unmarked_days, PlayerNotFoundError
@@ -36,7 +37,7 @@ def main():
     log = logging.getLogger(__name__)
 
     log.info("Logging configured at %s", logging.getLevelName(log_level))
-    
+
     if args.dry_run:
         log.info("Dry run mode is enabled: real email won't be sent")
 
@@ -75,10 +76,14 @@ def main():
                     f"DRY RUN: would send email: subject={subject} body={body}"
                 )
             else:
-                send_email(subject=subject, body=body,)
-                log.info(
-                    f"Found unmartked days for '{player_name}', email sent"
-                )
+                try:
+                    send_email(subject=subject, body=body,)
+                    log.info(
+                        f"Found unmarked days for '{player_name}', email sent"
+                    )
+                except RequestException as e:
+                    log.error(f"Failed to send email: {e}")
+                    sys.exit(3)
         else:
             log.info(
                 f"All days are marked for '{player_name}', email not sent"
